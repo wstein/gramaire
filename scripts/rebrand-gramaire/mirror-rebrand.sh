57b88c48e73c7a4374f32ccb71fbea111ed40a82
Werner Stein <werner.stein@gmail.com>
2026-07-06 21:12:42 +0200

    fix: update test to check for filename callback instead of file info callback

--- scripts/rebrand-gramaire/mirror-rebrand.sh
+++ scripts/rebrand-gramaire/mirror-rebrand.sh
@@ -105,7 +105,6 @@ COMMIT_MSG_CALLBACK_CODE="import subprocess, sys; return subprocess.check_output
 
 git -C "$MIRROR_DIR" filter-repo \
   --filename-callback "$(cat "$SCRIPT_DIR/rename_paths_callback.py")" \
-  --file-info-callback "$(cat "$SCRIPT_DIR/file_info_callback.py")" \
   --message-callback "$COMMIT_MSG_CALLBACK_CODE"
 
 git -C "$MIRROR_DIR" filter-repo \
